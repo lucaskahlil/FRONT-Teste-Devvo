7b9ad2fd3a5987b3982e2d63828f36d6becda06b
lucaskahlil <kahlilpereira@gmail.com>
2024-09-13 12:44:27 -0300

    remove mock data

--- rings-Devvo/src/pages/colection/index.tsx
+++ rings-Devvo/src/pages/colection/index.tsx
@@ -1,65 +1,8 @@
 import { Header } from "../../components/header"
 import backgroundImage from "../../assets/BackgroundColection.png"
 import { RingCarousel } from "../../components/carousels/RingCarousel"
-import { IRingDTOSchema } from "../../types"
 
 export function Colection() {
-    const rings: IRingDTOSchema[] = [
-        {
-            id: 1,
-            name: "Anel do Fogo",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlSw3Zm06mTNt5FU675Ilp_awLBGWOLkYsww&s",
-            type: "HUMAN"
-        },
-        {
-            id: 2,
-            name: "Anel do Fogo 2",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://images.unsplash.com/photo-1621826081161-5b3f6c1f7d0b",
-            type: "HUMAN"
-        },
-        {
-            id: 3,
-            name: "Anel do Fogo 3",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://images.unsplash.com/photo-1621826081161-5b3f6c1f7d0b",
-            type: "HUMAN"
-        },
-        {
-            id: 4,
-            name: "Anel do Fogo 4",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://images.unsplash.com/photo-1621826081161-5b3f6c1f7d0b",
-            type: "HUMAN"
-        },
-        {
-            id: 5,
-            name: "Anel do Fogo 5",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://images.unsplash.com/photo-1621826081161-5b3f6c1f7d0b",
-            type: "HUMAN"
-        },
-        {
-            id: 6,
-            name: "Anel do Fogo 6",
-            power: "Fogo",
-            forger: "Gandalf",
-            ringBearer: "Frodo",
-            image: "https://images.unsplash.com/photo-1621826081161-5b3f6c1f7d0b",
-            type: "HUMAN"
-        }
-    ]
 
     return (
         <div
@@ -73,7 +16,7 @@ export function Colection() {
                 </section>
 
                 <section className=" mpt-6 pb-6 pt-6 w-full h-full flex align-middle justify-center overflow-hidden ">
-                    <RingCarousel rings={rings} />
+                    <RingCarousel />
                 </section>
             </div>
         </div>
