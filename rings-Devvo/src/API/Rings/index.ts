75b8793827f4db43de32073850f2f40122facc08
lucaskahlil <kahlilpereira@gmail.com>
2024-09-13 12:45:07 -0300

    implements erros

--- rings-Devvo/src/API/Rings/index.ts
+++ rings-Devvo/src/API/Rings/index.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { IRingDTOSchema, IRingFormSchema } from "../../types";
 import { api } from "../api";
 
@@ -33,8 +33,34 @@ export const apiRings = {
       );
       return response.data;
     } catch (error) {
-      console.error(error);
-      throw new Error("Erro ao criar anel");
+      if (error instanceof AxiosError) {
+        switch (error.response?.status) {
+          case 400:
+            // Bad Request
+            throw new Error("Dados inválidos. Verifique e tente novamente.");
+          case 401:
+            // Unauthorized
+            throw new Error("Não autorizado. Faça login novamente.");
+          case 404:
+            // Not Found
+            throw new Error(
+              "Falha ao conectar com o servidor. Tente novamente mais tarde."
+            );
+          case 500:
+            // Internal Server Error
+            throw new Error(
+              "Erro interno do servidor. Tente novamente mais tarde."
+            );
+          default:
+            // Outros erros
+            throw new Error(
+              error.response?.data?.message || "Erro desconhecido ao criar anel"
+            );
+        }
+      } else {
+        // Erro desconhecido
+        throw new Error("Erro desconhecido ao criar anel");
+      }
     }
   },
 
