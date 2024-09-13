c1c81c5de1fe34252d39bbb8214102bfa4d91b16
lucaskahlil <kahlilpereira@gmail.com>
2024-09-13 20:19:34 -0300

    update RingForm

--- rings-Devvo/src/components/forms/RingForm/index.tsx
+++ rings-Devvo/src/components/forms/RingForm/index.tsx
@@ -12,7 +12,6 @@ const RingFormSchema = Yup.object().shape({
     type: Yup.string()
         .oneOf([typeRingEnum[0], typeRingEnum[1], typeRingEnum[2], typeRingEnum[3]], "Tipo inválido")
         .required("Tipo de anel é obrigatório"),
-    image: Yup.mixed().nullable().required("Imagem é obrigatória"),
 });
 
 const RingForm: React.FC = () => {
@@ -26,72 +25,57 @@ const RingForm: React.FC = () => {
                 ringBearer: "",
                 forger: "",
                 type: typeRingEnum[0],
-                image: null,
             }}
             validationSchema={RingFormSchema}
             onSubmit={async (values) => {
                 await createRing(values);
             }}
         >
-            {({ setFieldValue }) => (
-                <Form className="max-w-[500px] flex flex-col gap-5 mt-6">
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="name" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Nome do Anel</label>
-                        <Field
-                            name="name"
-                            type="text"
-                            className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500"
-                        />
-                        <ErrorMessage name="name" component="div" className="text-red-400" />
-                    </div>
 
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="power" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Poder</label>
-                        <Field name="power" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
-                        <ErrorMessage name="power" component="div" className="text-red-400" />
-                    </div>
+            <Form className="max-w-[500px] flex flex-col gap-5 mt-6">
+                <div className="flex flex-col gap-2 justify-normal">
+                    <label htmlFor="name" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Nome do Anel</label>
+                    <Field
+                        name="name"
+                        type="text"
+                        className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500"
+                    />
+                    <ErrorMessage name="name" component="div" className="text-red-400" />
+                </div>
 
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="ringBearer" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Portador do Anel</label>
-                        <Field name="ringBearer" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
-                        <ErrorMessage name="ringBearer" component="div" className="text-red-400" />
-                    </div>
+                <div className="flex flex-col gap-2 justify-normal">
+                    <label htmlFor="power" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Poder</label>
+                    <Field name="power" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
+                    <ErrorMessage name="power" component="div" className="text-red-400" />
+                </div>
 
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="forger" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Forjador</label>
-                        <Field name="forger" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
-                        <ErrorMessage name="forger" component="div" className="text-red-400" />
-                    </div>
+                <div className="flex flex-col gap-2 justify-normal">
+                    <label htmlFor="ringBearer" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Portador do Anel</label>
+                    <Field name="ringBearer" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
+                    <ErrorMessage name="ringBearer" component="div" className="text-red-400" />
+                </div>
 
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="type" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Tipo de Anel</label>
-                        <Field name="type" as="select" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500">
-                            <option value="HUMAN">Humano</option>
-                            <option value="ELF">Elfo</option>
-                            <option value="DWARF">Anão</option>
-                            <option value="SAURON">Sauron</option>
-                        </Field>
-                        <ErrorMessage name="type" component="div" className="text-red-400" />
-                    </div>
+                <div className="flex flex-col gap-2 justify-normal">
+                    <label htmlFor="forger" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Forjador</label>
+                    <Field name="forger" type="text" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500" />
+                    <ErrorMessage name="forger" component="div" className="text-red-400" />
+                </div>
 
-                    <div className="flex flex-col gap-2 justify-normal">
-                        <label htmlFor="image" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Imagem</label>
-                        <input
-                            name="image"
-                            type="file"
-                            onChange={(event) => {
-                                setFieldValue("image", event.currentTarget.files);
-                            }}
-                        />
-                        <ErrorMessage name="image" component="div" className="text-red-400" />
-                    </div>
+                <div className="flex flex-col gap-2 justify-normal">
+                    <label htmlFor="type" className="font-inter text-sm font-medium leading-[18.48px] text-left text-white">Tipo de Anel</label>
+                    <Field name="type" as="select" className="bg-zinc-800 h-12 pl-3 pr-3 rounded-lg text-white border border-solid border-gray-500">
+                        <option value="HUMAN">Humano</option>
+                        <option value="ELF">Elfo</option>
+                        <option value="DWARF">Anão</option>
+                        <option value="SAURON">Sauron</option>
+                    </Field>
+                    <ErrorMessage name="type" component="div" className="text-red-400" />
+                </div>
 
-                    <Button type="submit" disabled={loading} className="bg-yellow-200 text-black hover:text-white">
-                        Forjar Anel
-                    </Button>
-                </Form>
-
-            )}
+                <Button type="submit" disabled={loading} className="bg-yellow-200 text-black hover:text-white">
+                    Forjar Anel
+                </Button>
+            </Form>
         </Formik>
     );
 };
