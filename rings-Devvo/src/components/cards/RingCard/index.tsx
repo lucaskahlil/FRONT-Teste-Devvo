bd7b72948420271df11df8b4ee36f87bf5bfb639
lucaskahlil <kahlilpereira@gmail.com>
2024-09-13 07:46:09 -0300

    att style ringcard

--- rings-Devvo/src/components/cards/RingCard/index.tsx
+++ rings-Devvo/src/components/cards/RingCard/index.tsx
@@ -1,20 +1,20 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../ui/card";
+import { Card, CardContent, CardDescription } from "../../ui/card";
 import { IRingCardProps } from "./interface";
 
 export function RingCard({ ring }: IRingCardProps) {
     return (
-        <Card className="max-w-52">
-            <CardHeader>
-                <CardTitle>{ring.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <img src={ring.image} alt="Imagem do anel" />
-                <CardDescription>
-                    <p>Power: {ring.power}</p>
-                    <p>Bearer: {ring.ringBearer}</p>
-                    <p>Forger: {ring.forger}</p>
+        <Card className="w-96 border border-gray-900 rounded-xl bg-zinc-950">
+
+            <CardContent className="border border-gray-300 rounded-xl flex flex-col gap-5 p-0">
+                < img src={ring.image} alt="Imagem do anel" className="h-[270px] rounded-t-xl" />
+                <CardDescription className="font-inter text-[16px] font-normal leading-[22.4px] text-left text-yellow-100 flex flex-col gap-3 pl-4 pb-3 pr-4 pt-3">
+                    <p><strong>Nome:</strong> {ring.name}</p>
+                    <p><strong>Poder:</strong> {ring.power}</p>
+                    <p><strong>Portador:</strong> {ring.ringBearer}</p>
+                    <p><strong>Forjador:</strong> {ring.forger}</p>
+                    <p><strong>Tipo:</strong> {ring.type}</p>
                 </CardDescription>
-            </CardContent>
-        </Card>
+            </CardContent >
+        </Card >
     );
 }
